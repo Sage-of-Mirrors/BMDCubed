e7abcc04ebb9309c9f89f76ddcab9e56bb94da63
Sage_of_Mirrors <empressglatia@gmail.com>
2017-01-29 16:07:17 -0600

    Added basic command line interface.

--- BMDCubed/Program.cs
+++ BMDCubed/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using grendgine_collada;
+using System.IO;
 
 namespace BMDCubed
 {
@@ -10,6 +12,47 @@ namespace BMDCubed
     {
         static void Main(string[] args)
         {
+            string inputFileName = "";
+            string outputFileName = "";
+
+            #region Get input/output filenames
+
+            if (args.Length != 0)
+            {
+                if (args.Length == 1)
+                {
+                    inputFileName = args[0];
+                }
+                else if (args.Length == 2)
+                {
+                    inputFileName = args[0];
+                    outputFileName = args[1];
+                }
+            }
+            else
+            {
+                DisplayHelpMessage();
+                return;
+            }
+
+            if (outputFileName == "")
+            {
+                outputFileName = string.Format("{0}\\{1}.bmd", Path.GetDirectoryName(inputFileName), 
+                                                               Path.GetFileNameWithoutExtension(inputFileName));
+            }
+
+            #endregion
+
+            Grendgine_Collada sourceModel = Grendgine_Collada.Grendgine_Load_File(inputFileName);
+
+
+        }
+
+        static void DisplayHelpMessage()
+        {
+            Console.WriteLine("BMDCubed written by Sage_of_Mirrors (@SageOfMirrors).");
+            Console.WriteLine("Thanks to LordNed and all who came before us.");
+            Console.WriteLine("Usage: BMDCubed input_file [output_file]");
         }
     }
 }
