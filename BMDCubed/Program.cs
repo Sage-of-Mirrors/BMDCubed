5922b3b1a8071697fceb4bc9c12fc75db89d6170
Dylan Ascencio <Dylan Ascencio>
2017-01-30 10:10:22 -0600

    Added support for getting skeleton root from the DAE, as well as BMDManager, which will manage conversion of DAE data to BMD.

--- BMDCubed/Program.cs
+++ BMDCubed/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using grendgine_collada;
 using System.IO;
+using BMDCubed.src;
+using GameFormatReader.Common;
 
 namespace BMDCubed
 {
@@ -17,24 +19,29 @@ namespace BMDCubed
 
             #region Get input/output filenames
 
+            // The user supplied at least an input file
             if (args.Length != 0)
             {
+                // Just an input file
                 if (args.Length == 1)
                 {
                     inputFileName = args[0];
                 }
+                // Input file and a file name to output to
                 else if (args.Length == 2)
                 {
                     inputFileName = args[0];
                     outputFileName = args[1];
                 }
             }
+            // User didn't give any files to convert, display help instead
             else
             {
                 DisplayHelpMessage();
                 return;
             }
 
+            // Output file name wasn't set. So we'll just make it the input file name and replace its extension with .bmd
             if (outputFileName == "")
             {
                 outputFileName = string.Format("{0}\\{1}.bmd", Path.GetDirectoryName(inputFileName), 
@@ -44,8 +51,13 @@ namespace BMDCubed
             #endregion
 
             Grendgine_Collada sourceModel = Grendgine_Collada.Grendgine_Load_File(inputFileName);
+            BMDManager manager = new BMDManager(sourceModel);
 
-
+            using (FileStream stream = new FileStream(outputFileName, FileMode.Create, FileAccess.Write))
+            {
+                EndianBinaryWriter writer = new EndianBinaryWriter(stream, Endian.Big);
+                manager.WriteBMD(writer);
+            }
         }
 
         static void DisplayHelpMessage()
---
+++ BMDCubed/src/BMD/Bone.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using grendgine_collada;
+
+namespace BMDCubed.src.BMD
+{
+    class Bone
+    {
+        // What these do are currently not known
+        public byte Unknown1;
+        public byte Unknown2;
+        public byte Unknown3;
+        public byte Unknown4;
+
+        public Vector3 Scale;
+        public Vector3 Translation;
+        public Vector3 Rotation;
+        public BoundingBox Bounds;
+
+        public string Name;
+
+        public List<Bone> Children;
+
+        public Bone(Grendgine_Collada_Node node)
+        {
+            Children = new List<Bone>();
+
+            Name = node.Name;
+
+            float[] nodeTransform = node.Matrix[0].Value();
+            Matrix4 transform = new Matrix4(nodeTransform[0], nodeTransform[1], nodeTransform[2], nodeTransform[3],
+                                            nodeTransform[4], nodeTransform[5], nodeTransform[6], nodeTransform[7],
+                                            nodeTransform[8], nodeTransform[9], nodeTransform[10], nodeTransform[11],
+                                            nodeTransform[12], nodeTransform[13], nodeTransform[14], nodeTransform[15]);
+
+            Scale = transform.ExtractScale();
+            Translation = new Vector3(transform.Column3[0], transform.Column3[1], transform.Column3[2]);
+            Rotation = transform.ExtractRotation().Xyz;
+
+            if (node.node == null)
+                return;
+
+            foreach (Grendgine_Collada_Node childNode in node.node)
+            {
+                Bone child = new Bone(childNode);
+                Children.Add(child);
+            }
+        }
+    }
+}
